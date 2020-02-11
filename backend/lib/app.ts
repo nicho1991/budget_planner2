d81f360fd4d89bbf9af4d5303a0ae6de6772fffd
Nicholas <nicholas@flightlogger.net>
2020-02-11 20:43:33 +0100

    routing setup

--- backend/lib/app.ts
+++ backend/lib/app.ts
@@ -1,13 +1,16 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes/crmRoutes";
 
 class App {
 
     public app: express.Application;
+    public routePrv: Routes = new Routes();
 
     constructor() {
         this.app = express();
         this.config();        
+        this.routePrv.routes(this.app);  
     }
 
     private config(): void{
@@ -16,7 +19,6 @@ class App {
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
-
 }
 
 export default new App().app;
