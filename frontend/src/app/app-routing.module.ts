a8d431e1806f3879506703f321b981e9b44a2d13
Nicholas <nicholas@flightlogger.net>
2020-02-11 20:31:32 +0100

    new backend rest

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponentComponent } from './components/hello-component/hello-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'hello' , component: HelloComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponentComponent } from './components/hello-component/hello-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponentComponent
   ],
   imports: [
     BrowserModule,
